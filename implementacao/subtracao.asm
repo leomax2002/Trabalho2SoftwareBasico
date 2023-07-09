9c434a7cbba3a64ae5b700d24c7a9c61c9bcea14
wawa <wallacebtlw@gmail.com>
2023-07-09 17:32:11 -0300

    subtracao 32

--- implementacao/subtracao.asm
+++ implementacao/subtracao.asm
@@ -9,6 +9,9 @@ section .text
     extern print_num_16
     extern print_num_32
 
+%define local1 dword [ebp-4]
+%define local2 dword [ebp-8]
+
 subtracao16:
     enter 0, 0
     ; lê um numero
@@ -22,9 +25,17 @@ subtracao16:
     ret
 
 subtracao32:
-    enter 0, 0
-    ; lê um numero
+    enter 8, 0 ; 2 variaveis locais
+
+    ; lê os numeros
     call read_num_32
+    mov local1, eax
+
+    call read_num_32
+    mov local2, eax
+
+    mov eax, local1
+    sub eax, local2
 
     ; imprime um numero
     push eax
