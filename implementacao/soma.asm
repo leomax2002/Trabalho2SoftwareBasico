93f02ee7f42d8e672df94a8fa9b19c84eaba8d9b
wawa <wallacebtlw@gmail.com>
2023-07-09 17:28:46 -0300

    sum32 working

--- implementacao/soma.asm
+++ implementacao/soma.asm
@@ -9,6 +9,9 @@ section .text
     extern print_num_16
     extern print_num_32
 
+%define local1 dword [ebp-4]
+%define local2 dword [ebp-8]
+
 soma16:
     enter 0, 0
     ; lê um numero
@@ -22,9 +25,17 @@ soma16:
     ret
 
 soma32:
-    enter 0, 0
-    ; lê um numero
+    enter 8, 0 ; 2 variaveis locais
+
+    ; lê os numeros
     call read_num_32
+    mov local1, eax
+
+    call read_num_32
+    mov local2, eax
+
+    mov eax, local1
+    add eax, local2
 
     ; imprime um numero
     push eax
